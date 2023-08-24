81029c9b2a6530be578031d9837df7be08c9dc2e
hyeg0121 <hyeg0121@naver.com>
2023-08-24 22:30:15 +0900

    init

---
+++ src/test/java/com/example/kotlinprojectserver/KotlinProjectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kotlinprojectserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotlinProjectServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
