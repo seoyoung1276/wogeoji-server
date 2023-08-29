9726d02220acf32ffb52dc6f25d36482f20ebd15
seoyoung1276 <seoyoung1276@gmail.com>
2023-08-29 20:05:51 +0900

    entity

---
+++ src/main/java/com/example/kotlinprojectserver/entity/member.java
@@ -0,0 +1,21 @@
+package com.example.kotlinprojectserver.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class member {
+    @Id
+    @GeneratedValue
+    private Long userNumber; //pk
+    private String name;
+    private String id;
+    private String password;
+    private String email;
+
+}
