b09f1c64f1df6c6550396db8d4a498bde122043e
hyeg <hyeg0121@naver.com>
2023-08-31 19:22:10 +0900

    Member entity 추가

--- src/main/java/com/example/kotlinprojectserver/entity/Member.java
+++ src/main/java/com/example/kotlinprojectserver/entity/Member.java
@@ -1,21 +1,40 @@
 package com.example.kotlinprojectserver.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Set;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Member {
     @Id
-    @GeneratedValue
-    private Long userNumber; //pk
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long memberId; //pk
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "id")
     private String id;
+
+    @Column(name = "pw")
     private String password;
+
+    @Column(name = "email")
     private String email;
+
+    @ManyToMany
+    @JoinTable(
+        name = "user_group",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "group_id")
+    )
+    private Set<Group> groups;
 
 }
