8a729472069d330a2f8e3d5b0107aedfd5e1eddb
hyeg <hyeg0121@naver.com>
2023-08-31 19:22:24 +0900

    Member repository 추가

---
+++ src/main/java/com/example/kotlinprojectserver/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.example.kotlinprojectserver.repository;
+
+import com.example.kotlinprojectserver.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
