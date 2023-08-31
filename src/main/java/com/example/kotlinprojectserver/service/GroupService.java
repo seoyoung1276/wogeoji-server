120bfc07c49c9193f0662616d2c68378138b54e6
hyeg <hyeg0121@naver.com>
2023-08-31 19:55:35 +0900

    group service 추가

---
+++ src/main/java/com/example/kotlinprojectserver/service/GroupService.java
@@ -0,0 +1,44 @@
+package com.example.kotlinprojectserver.service;
+
+import com.example.kotlinprojectserver.entity.Group;
+import com.example.kotlinprojectserver.repository.GroupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class GroupService {
+    private final GroupRepository groupRepository;
+
+    @Autowired
+    public GroupService(GroupRepository groupRepository){
+        this.groupRepository = groupRepository;
+    }
+
+    public List<Group> getAllGroups() {
+        return groupRepository.findAll();
+    }
+
+    public Optional<Group> getGroupById(Long groupId) {
+        return groupRepository.findById(groupId);
+    }
+
+    public Group createGroup(Group group) {
+        return groupRepository.save(group);
+    }
+
+    public Group updateGroup(Long groupId, Group updateGroup) {
+        if (groupRepository.existsById(groupId)) {
+            updateGroup.setGroupId(groupId);
+            return groupRepository.save(updateGroup);
+        } else {
+            throw new RuntimeException("Group not found with ID: " + groupId);
+        }
+    }
+
+    public void deleteGroup(Long groupId) {
+        groupRepository.deleteById(groupId);
+    }
+}
