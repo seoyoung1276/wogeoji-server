8428a637399ac082379e3fbf67606b259da3ba1c
seoyoung1276 <seoyoung1276@gmail.com>
2023-09-12 20:06:00 +0900

    글 작성 api

---
+++ src/main/java/com/example/kotlinprojectserver/controller/BlogApiController.java
@@ -0,0 +1,25 @@
+package com.example.kotlinprojectserver.controller;
+
+import com.example.kotlinprojectserver.dto.AddArticleRequest;
+import com.example.kotlinprojectserver.entity.Article;
+import com.example.kotlinprojectserver.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
+public class BlogApiController {
+
+    private final BlogService blogService;
+
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
+        Article savedArticle = blogService.save(request);
+
+        return  ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+    }
+}
---
+++ src/main/java/com/example/kotlinprojectserver/entity/Article.java
@@ -0,0 +1,33 @@
+package com.example.kotlinprojectserver.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.ErrorResponse;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false) // 'title' 이라는 not null 컬럼과 매핑
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder
+    public Article(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
+
+}
