013d19e224f760e9f47144782457586dad002d8e
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-10 19:33:48 +0900

    findByMemberIdAndPassword

--- src/main/java/com/example/kotlinprojectserver/repository/MemberRepository.java
+++ src/main/java/com/example/kotlinprojectserver/repository/MemberRepository.java
@@ -4,7 +4,9 @@ import com.example.kotlinprojectserver.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
+    Optional<Member> findByMemberIdAndPassword(String memberId, String password);
 }
