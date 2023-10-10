8a59a81c024ec3e5db13bc73bdb2fa24534951af
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-10 19:34:23 +0900

    로그인 auth 컨트롤러

---
+++ src/main/java/com/example/kotlinprojectserver/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.example.kotlinprojectserver.controller;
+
+import com.example.kotlinprojectserver.entity.Member;
+import com.example.kotlinprojectserver.repository.MemberRepository;
+import com.example.kotlinprojectserver.request.LoginRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public AuthController(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Optional<Member>> login(@RequestBody LoginRequest loginRequest) {
+        Optional<Member> member = memberRepository.findByMemberIdAndPassword(loginRequest.getMemberId(), loginRequest.getPassword());
+
+        if (member.isPresent()) {
+            System.out.println(1);
+            System.out.println(member);
+            return ResponseEntity.ok(member);
+        } else {
+            System.out.println(0);
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+}
