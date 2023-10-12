a9e46b705c5bd85cc27d2957a42efa40377342ed
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-12 22:33:14 +0900

    응답 dto에 그룹 리스트 추가

--- src/main/java/com/example/kotlinprojectserver/dto/MemberResponse.java
+++ src/main/java/com/example/kotlinprojectserver/dto/MemberResponse.java
@@ -1,7 +1,10 @@
 package com.example.kotlinprojectserver.dto;
 
+import com.example.kotlinprojectserver.entity.Group;
 import com.example.kotlinprojectserver.entity.Member;
 import lombok.Getter;
+
+import java.util.Set;
 
 @Getter
 public class MemberResponse {
@@ -10,12 +13,14 @@ public class MemberResponse {
     private final String memberId;
     private final String password;
     private final String email;
+    private final Set<Group> groups;
 
     public MemberResponse(Member member) {
-        this.memberNo = getMemberNo();
+        this.memberNo = member.getMemberNo();
         this.name = member.getName();
         this.memberId = member.getMemberId();
         this.password = member.getPassword();
         this.email = member.getEmail();
+        this.groups = member.getGroups();
     }
 }
