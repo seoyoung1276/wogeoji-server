ded241535d58f3e17f0bf61ed8f6a983a6eee68c
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-24 20:11:15 +0900

    feat: article repository

---
+++ src/main/java/com/example/kotlinprojectserver/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.example.kotlinprojectserver.repository;
+
+import com.example.kotlinprojectserver.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
