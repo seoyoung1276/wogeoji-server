9902fb2753e8677b2ba55fdbb293530c3e78e19e
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-24 20:13:24 +0900

    feat: 유저가 그룹에 참여하는 api

--- src/main/java/com/example/kotlinprojectserver/controller/GroupController.java
+++ src/main/java/com/example/kotlinprojectserver/controller/GroupController.java
@@ -4,7 +4,9 @@ import com.example.kotlinprojectserver.dto.GroupResponse;
 import com.example.kotlinprojectserver.dto.MemberResponse;
 import com.example.kotlinprojectserver.entity.Group;
 import com.example.kotlinprojectserver.entity.Member;
+import com.example.kotlinprojectserver.request.GroupRequest;
 import com.example.kotlinprojectserver.service.GroupService;
+import com.example.kotlinprojectserver.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,10 +20,12 @@ import java.util.Optional;
 public class GroupController {
 
     private final GroupService groupService;
+    private final MemberService memberService;
 
     @Autowired
-    public GroupController(GroupService groupService) {
+    public GroupController(GroupService groupService, MemberService memberService) {
         this.groupService = groupService;
+        this.memberService = memberService;
     }
 
     @GetMapping("/")
@@ -40,12 +44,39 @@ public class GroupController {
     }
 
     @PostMapping("/group")
-    public ResponseEntity<Group> createMember(@RequestBody Group group) {
-        Group savedGroup = groupService.createGroup(group);
+    public ResponseEntity<Group> createMember(@RequestBody GroupRequest group) {
+        Group savedGroup = groupService.createGroup(new Group(group.getName(), group.getDescription(), group.getHeadcountLimit()));
         return ResponseEntity.status(HttpStatus.CREATED).body(savedGroup);
     }
     @PutMapping("/{groupID}")
     public void deleteGroup(@PathVariable Long groupID) {
         groupService.deleteGroup(groupID);
+    }
+
+    @PostMapping("/{groupId}/join")
+    public ResponseEntity<String> joinGroup(
+            @PathVariable Long groupId,
+            @RequestParam Long memberId
+    ) {
+        Group group = groupService.getGroupById(groupId);
+        Member member = memberService.getMemberById(memberId);
+
+        if (group != null && member != null) {
+            if (!group.getUsers().contains(member)) {
+                group.getUsers().add(member); // Add the member to the group
+                groupService.saveGroup(group);
+
+                if (!member.getGroups().contains(group)) {
+                    member.getGroups().add(group);
+                    memberService.saveMember(member);
+                }
+
+                return ResponseEntity.ok("Member has joined the group.");
+            } else {
+                return ResponseEntity.badRequest().body("Member is already part of the group.");
+            }
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
